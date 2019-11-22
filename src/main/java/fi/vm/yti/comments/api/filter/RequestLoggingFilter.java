763ff56b05a4849bc297e69bc76b3240bc4f777e
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-22 07:39:56 +0200

    Minor refactor to method name in request header logging.

--- src/main/java/fi/vm/yti/comments/api/filter/RequestLoggingFilter.java
+++ src/main/java/fi/vm/yti/comments/api/filter/RequestLoggingFilter.java
@@ -42,7 +42,7 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
             LOG.debug("User: anonymous");
         }
         logQueryParameters(requestContext);
-        logRequestHeader(requestContext);
+        logRequestHeaders(requestContext);
     }
 
     private void logQueryParameters(final ContainerRequestContext requestContext) {
@@ -54,7 +54,7 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
         LOG.debug("*** End query parameters section of request ***");
     }
 
-    private void logRequestHeader(final ContainerRequestContext requestContext) {
+    private void logRequestHeaders(final ContainerRequestContext requestContext) {
         LOG.debug("*** Start header section of request ***");
         LOG.debug("Method type: {}", requestContext.getMethod());
         requestContext.getHeaders().keySet().forEach(headerName -> {
